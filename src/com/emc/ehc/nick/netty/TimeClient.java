1d0b3f81a98bab15ffacdc19a343ffb677612f18
NickNYU <cz739@nyu.edu>
2016-03-18 08:44:17 +0800

    03/18/2016

--- src/com/emc/ehc/nick/netty/TimeClient.java
+++ src/com/emc/ehc/nick/netty/TimeClient.java
@@ -1,6 +1,9 @@
 package com.emc.ehc.nick.netty;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -8,15 +11,30 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import java.nio.channels.*;
 
 public class TimeClient {
-	public void connect(int port) {
+	public void connect(String host, int port) throws InterruptedException {
 		EventLoopGroup group = new NioEventLoopGroup();
 		try {
 			Bootstrap b = new Bootstrap();
 			b.group(group).channel(NioSocketChannel.class)
 				.option(ChannelOption.TCP_NODELAY, true)
-				.handler();
-		} finally {
+				.handler(new ChannelInitializer(){
+
+					@Override
+					protected void initChannel(Channel ch) throws Exception {
+						ch.pipeline().addLast(new TimeClientHandler());
+					}
+					
+				});
+			ChannelFuture f = b.connect(host, port).sync();
 			
+			f.channel().closeFuture().sync();
+		} finally {
+			group.shutdownGracefully();
 		}
+	}
+	
+	public static void main(String[] args) {
+		int port = 8080;
+		
 	}
 }
