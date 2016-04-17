57829fe80d5f1e82bd2f1c5717bdf90b1f18cc83
NickNYU <cz739@nyu.edu>
2016-04-17 22:53:47 +0800

    增加修改

---
+++ src/com/emc/ehc/nick/netty/heartbeat/PingMsg.java
@@ -0,0 +1,13 @@
+package com.emc.ehc.nick.netty.heartbeat;
+/** 
+* @author Nick Zhu
+* @email  cz739@nyu.edu 
+* @version 创建时间：2016年4月17日 下午10:52:55 
+* 
+*/
+public class PingMsg extends AbstractMsg {
+	public PingMsg() {
+		super();
+		setType(MsgType.PING);
+	}
+}
