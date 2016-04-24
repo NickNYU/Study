129294bfb5671ccd5732d15b076395fd8dea9c01
NickNYU <cz739@nyu.edu>
2016-04-24 09:25:58 +0800

    开始自己实现完整的Reactor

---
+++ src/com/emc/ehc/nick/netty/Reactor/Advanced/ChannelHandler.java
@@ -0,0 +1,10 @@
+package com.emc.ehc.nick.netty.Reactor.Advanced;
+/** 
+* @author Nick Zhu
+* @email  cz739@nyu.edu 
+* @version 创建时间：2016年4月24日 上午9:25:03 
+* 
+*/
+public interface ChannelHandler {
+
+}
