94f662a3349450c3ff3e15d46629daeae3ee7edd
NickNYU <cz739@nyu.edu>
2016-04-24 11:03:42 +0800

    Reactor多路复用

--- src/com/emc/ehc/nick/netty/Reactor/Advanced/Reactor.java
+++ src/com/emc/ehc/nick/netty/Reactor/Advanced/Reactor.java
@@ -1,6 +1,10 @@
 package com.emc.ehc.nick.netty.Reactor.Advanced;
 
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.util.Iterator;
+import java.util.Set;
 
 /** 
 * @author Nick Zhu
@@ -12,6 +16,35 @@ public abstract class Reactor implements Runnable {
 	
 	@Override
 	public void run() {
+		while(!Thread.interrupted()) {
+			for(Selector selector : selectors) {
+				try {
+					int selectedNum = selector.select();
+					if(selectedNum == 0) {
+						continue;
+					}
+					Set<SelectionKey> selectedKeys = selector.selectedKeys();
+					Iterator<SelectionKey> it = selectedKeys.iterator();
+					
+					for(int i = 0; i < selectedNum; i++) {
+						dispatch((SelectionKey) it.next());
+					}
+					
+					selectedKeys.clear();
+				} catch (IOException e) {
+					try {
+						selector.close();
+						selector = Selector.open();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					
+				}
+			}
+		}
+	}
+	
+	public void dispatch(SelectionKey key) {
 		
 	}
 	
@@ -20,4 +53,15 @@ public abstract class Reactor implements Runnable {
 	//默认是1
 	private int selectorNum = 1;
 	
+	public Reactor() throws IOException {
+		this(1);
+	}
+	
+	public Reactor(int num) throws IOException {
+		this.selectorNum = num;
+		selectors = new Selector[selectorNum];
+		for(int i = 0; i < selectorNum; i++) {
+			selectors[i] = Selector.open();
+		}
+	}
 }
