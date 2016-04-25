9f1492ee7240a8a70f545a31bd5caf1eb7770588
NickNYU <cz739@nyu.edu>
2016-04-25 22:46:05 +0800

    完成client

--- src/com/emc/ehc/nick/netty/mtu/Client.java
+++ src/com/emc/ehc/nick/netty/mtu/Client.java
@@ -1,4 +1,14 @@
 package com.emc.ehc.nick.netty.mtu;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+
 /** 
 * @author Nick Zhu
 * @email  cz739@nyu.edu 
@@ -6,5 +16,40 @@ package com.emc.ehc.nick.netty.mtu;
 * 
 */
 public class Client {
+	
+	public static void main(String[] args) throws InterruptedException {
+		new Client("localhost", 8090).run();
+	}
+	
+	private String host;
+	private int port;
+	
+	public Client(String host, int port) {
+		this.host = System.getProperty("host", host);
+		this.port = port;
+	}
+	
+	public void run() throws InterruptedException {
+		EventLoopGroup group = new NioEventLoopGroup();
+		try {
+			Bootstrap b = new Bootstrap();
+			b.group(group);
+			b.channel(NioSocketChannel.class)
+			.option(ChannelOption.TCP_NODELAY, true);
+			b.handler(new ChannelInitializer<SocketChannel> () {
 
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(new ClientHandler());
+				}
+				
+			});
+			
+			ChannelFuture f = b.connect(this.host, this.port).sync();
+			f.channel().closeFuture().sync();
+			
+		} finally {
+			group.shutdownGracefully();
+		}
+	}
 }
